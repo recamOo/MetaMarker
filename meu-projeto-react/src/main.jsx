471290bf0cebda5fa9ee538966968b2c1680f903
recamOo <lopesrodrigo942@gmail.com>
2026-06-21 12:07:34 -0300

    Arrumado o CSS 1.2.1

--- meu-projeto-react/src/main.jsx
+++ meu-projeto-react/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './GoalTracker.jsx'
 import App from './GoalTracker.jsx'
 
 createRoot(document.getElementById('root')).render(
